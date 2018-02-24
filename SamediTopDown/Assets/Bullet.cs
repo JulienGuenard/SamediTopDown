fcfcbcea12035544d1fc945b718219df9164a681
JulienGuenard <julienguenard01@gmail.com>
2018-02-24 15:31:09 +0100

    lol6

---
+++ SamediTopDown/Assets/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Bullet : NetworkBehaviour {
+
+  SpriteRenderer spriteR;
+
+	void Start () {
+      spriteR = GetComponent<SpriteRenderer>();
+
+      if (isLocalPlayer)
+      {
+        spriteR.material.color = Color.blue;
+      }else{
+          spriteR.material.color = Color.red;
+      }
+	}
+
+    void OnCollisionEnter (Collision collision)
+ {
+    Destroy(gameObject);
+ }
+ }
--- SamediTopDown/Assets/PlayerController.cs
+++ SamediTopDown/Assets/PlayerController.cs
@@ -6,10 +6,42 @@ using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour {
 
-	void Update () {
-    if (isLocalPlayer) {
-          transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+SpriteRenderer spriteR;
 
-    }
+public GameObject bulletPrefab;
+public Vector3 bulletSpawn;
+
+void Start()
+  {
+    spriteR = GetComponent<SpriteRenderer>();
+    if (isLocalPlayer)
+      {
+        spriteR.material.color = Color.blue;
+      }else{
+          spriteR.material.color = Color.red;
+      }
+}
+
+	void Update () 
+    {
+    if (isLocalPlayer) {
+          transform.position += new Vector3(Input.GetAxis("Horizontal")/2, 0, Input.GetAxis("Vertical")/2);
+
+          if (Input.GetKeyDown(KeyCode.Space)) 
+          {
+            CmdShoot();
+          }
+       }
 	}
+
+    [Server]
+    void CmdShoot () 
+    {
+    GameObject bullet = Instantiate(bulletPrefab, bulletSpawn, transform.rotation);
+
+    bullet.GetComponent<Rigidbody>().velocity = Vector3.forward;
+
+    NetworkServer.Spawn(bullet);
+      Destroy(gameObject,2.0f);
+    }
 }
