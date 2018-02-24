0d3254bc95c196575f139c1f6acec20443653b17
Unknown <oncheonche@ymail.com>
2018-02-24 16:28:13 +0100

    navidarium

--- SamediTopDown/Assets/Health.cs
+++ SamediTopDown/Assets/Health.cs
@@ -1,23 +1,35 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
 
-public class Health : MonoBehaviour
+public class Health : NetworkBehaviour
 {
     public const int maxHealth = 100;
+    [SyncVar(hook = "OnChangeHealth")]
     public int currentHealth = maxHealth;
     public Slider healthBar;
 
 
     public void TakeDamage(int amount)
     {
+        if (!isServer)
+        {
+            return;
+        }
+
         currentHealth -= amount;
         if (currentHealth <= 0)
         {
             currentHealth = 0;
             Debug.Log("Dead!");
         }
+        healthBar.value = currentHealth;
 
+    }
+    void OnChangeHealth(int health)
+    {
         healthBar.value = currentHealth;
     }
+
 }
