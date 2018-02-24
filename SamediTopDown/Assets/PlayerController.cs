d780ef710d63a91db1be06c8517c8ad070d7b3bd
JulienGuenard <julienguenard01@gmail.com>
2018-02-24 15:09:54 +0100

    jecommitlol3

--- SamediTopDown/Assets/PlayerController.cs
+++ SamediTopDown/Assets/PlayerController.cs
@@ -1,15 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+
 
 public class PlayerController : MonoBehaviour {
 
-Rigidbody rigidbody;
-
-void Start () {
-    rigidbody = GetComponent<Rigidbody>();
-}
-
 	void Update () {
-      rigidbody.velocity = new Vector3(Input.GetAxis("Horizontal")*100, Input.GetAxis("Vertical")*100,0); 
+    transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
 	}
 }
